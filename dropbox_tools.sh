df44047a7b1cffbdcc9934506c900017ed848808
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-03 14:19:19 -0400

    Create dropbox_tools.sh

---
+++ dropbox_tools.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Set new sudo password for kali user before running this script!"
+sleep 1
+
+if [ `id -u` -eq 0 ]
+then
+        echo "Running as root user :)"
+else
+        echo "Please run with sudo!"
+        exit 1
+fi
+
+echo "CLONING TOOLS INTO /opt"
+cd /opt
+
+echo "INSTALLING Impacket…"
+git clone https://github.com/SecureAuthCorp/impacket.git
+
+echo "INSTALLING Mitm6…"
+git clone https://github.com/fox-it/mitm6
+
+echo "INSTALLING PowerTools…"
+git clone https://github.com/PowerShellMafia/PowerSploit.git
+
+echo "INSTALLING SecLists…"
+git clone https://github.com/danielmiessler/SecLists.git
+
+echo "OPENING LINKS FOR OTHER TOOLS!"
+sleep 1
+echo "https://portswigger.net/burp/documentation/desktop/getting-started"
+
+echo "https://www.tenable.com/downloads/nessus"
+
+echo "https://addons.mozilla.org/en-US/firefox/addon/wappalyzer/"
+
+echo "https://addons.mozilla.org/en-US/firefox/addon/foxyproxy-basic/"
