f40147f56500a48d5ca2ef21a9d6a707bf258bf8
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-03 14:18:03 -0400

    Create azure_setup.sh

---
+++ azure_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Sudo Check
+if [ `id -u` -eq 0 ]
+then
+        echo "Running as root user :)"
+else
+        echo "Please run with sudo!"
+        exit 1
+fi
+
+cd ~
+echo "UPDATING…"
+apt update && apt upgrade -y
+
+echo "INSTALLING OPENVPN…"
+wget https://git.io/vpn -O openvpn-install.sh
+sudo chmod +x openvpn-install.sh
+sudo ./openvpn-install.sh
+
+systemctl enable openvpn
+echo "INSTALL COMPLETE! 
+echo "To add users or dropboxes please run sudo ./openvpn-install.sh and name the dropboxes dropbox1, dropbox2, etc…"
