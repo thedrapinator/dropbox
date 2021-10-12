ac91c1ec6b51d14270c36fb0ab76e7c7cdb19c2c
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-12 09:45:56 -0400

    Update wifi_tools.sh

--- wifi_tools.sh
+++ wifi_tools.sh
@@ -50,7 +50,7 @@ sudo ip link set wlan0 down; sudo iw dev wlan0 set type monitor; sudo ip link se
 ### Add eaphammer tools ####
 echo "INSTALLING EAP HAMMER!!!"
 cd /opt/
-git clone https://github.com/s0lst1c3/eaphammer.git
+sudo git clone https://github.com/s0lst1c3/eaphammer.git
 pip3 install pem
 cd /opt/eaphammer
 sudo ./kali-setup
