86ee04124bcd092f97d5e5dd708312450b0f5dc9
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-18 14:51:03 -0400

    Update dropbox_setup.sh

--- dropbox_setup.sh
+++ dropbox_setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+username=pentest
+filename=install.ovpn
+hostname=master
+
 #dropbox username
-echo "Please enter dropbox username! (usually kali or pentest)"
-read username
-echo "USERNAME = $username"
+#echo "Please enter dropbox username! (usually kali or pentest)"
+#read username
+#echo "USERNAME = $username"
+
 
 echo "Set new sudo password for $username user before running this script!"
 sleep 1
@@ -20,15 +25,15 @@ fi
 echo "Disabling Sleep"
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
-echo "Please enter the ovpn file name which will be copied from the /home/$username/ folder!"
-read filename
+#echo "Please enter the ovpn file name which will be copied from the /home/$username/ folder!"
+#read filename
 sudo cp /home/$username/$filename /etc/openvpn/openvpn.conf
 #sudo rm /home/$username/$filename
 sudo systemctl enable openvpn
 echo "Copied and enabled dropbox openvpn file!"
 
-echo "Please enter new hostname (companyname):"
-read hostname
+#echo "Please enter new hostname (companyname):"
+#read hostname
 sudo hostnamectl set-hostname $hostname
 echo "Hostname set to $hostname"
 
