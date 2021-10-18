bb78a8ef31a23e2167d319c08afd9de49f942302
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-18 13:30:41 -0400

    Create MASTER_INSTALL.sh

---
+++ MASTER_INSTALL.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Sudo Check
+if [ `id -u` -eq 0 ]
+then
+        echo "Running as user with sudo privs :)"
+else
+        echo "Please run with standard (NON ROOT) user and sudo!"
+        exit 1
+fi
+
+echo "###### CLONING LATEST FILES ######"
+cd /opt/
+sudo git clone https://github.com/sirchsec/dropbox.git
+cd /opt/dropbox
+sudo chmod +x /opt/dropbox/*
+
+echo "###### RUNNING DROPBOX SETUP SCRIPT ######"
+sudo /opt/dropbox/dropbox_setup.sh
+
+echo "###### RUNNING DROPBOX TOOLS SCRIPT ######"
+sudo dropbox_tools.sh
+
+echo "###### RUNNING WIFI TOOLS SCRIPT ######"
+sudo wifi_tools.sh
+
+echo "###### PLEASE REBOOT ######"
