06bd46e964bf569e1fd8bae777c48faea4fb7edc
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-10-28 14:00:46 -0400

    Update hostname_update.sh

--- hostname_update.sh
+++ hostname_update.sh
@@ -8,13 +8,13 @@ echo #### Setting Hostname ####
 echo "Enter New Host Name [dropbox name OR company name]"
 read -p 'Hostname: ' hostname
 sudo hostnamectl set-hostname $hostname
-sudo echo 127.0.0.1 localhost $(hostname) >> /etc/hosts   #new line to fix sudo issue
+sed -I "s/127.0.1.1.*/127.0.1.1\t$hostname/g" /etc/hosts
 echo "Hostname set to $hostname"
 
 ## OLD WAY
 #echo $newhostname > /etc/hostname
 #hostname -b $newhostname
-#sed -I "s/127.0.1.1.*/127.0.1.1\t$newhostname/g" /etc/hosts
+#sed -I "s/127.0.1.1.*/127.0.1.1\t$hostname/g" /etc/hosts
 
 
 ### Reboot Prompt
