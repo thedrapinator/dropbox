b955c38fee499ef6c35344c44fe31dc34ab7078f
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-11-15 15:55:02 -0500

    Create wifi_managed.sh

---
+++ wifi_managed.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+interface=wlan0
+echo "Interface is set to $interface"
+sleep 3
+
+if [ `id -u` -eq 0 ]
+then
+        echo "Running as sudo user :)"
+else
+        echo "Please run with sudo!"
+        exit 1
+fi
+
+
+echo "Configuring wifi adapter"   # Moved to the end
+sudo ip link set $interface down
+sudo iw $interface set txpower fixed 3000
+sudo iw dev $interface set type managed
+sudo ip link set $interface up
+
+echo "DONE!"
