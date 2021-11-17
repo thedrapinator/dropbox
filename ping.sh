cba46e02495aca69dc769e9a5010be8bee084e15
sirchsec <85844648+sirchsec@users.noreply.github.com>
2021-11-17 12:29:06 -0500

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -14,5 +14,5 @@ else
   date >> /opt/dropbox/log/pingfaillog.txt  #Write reboot date and time to log
   sleep 1   #small delay to ensure changes are saved
   #sudo systemctl restart openvpn  #Rebooting openvpn has same effect as reboot but maybe not if network manager is broken
-  /sbin/shutdown -r   #soft reboot
+  /sbin/shutdown -r now  #soft reboot
 fi
