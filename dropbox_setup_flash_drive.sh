96d6cdb45d29617b4723447bf0d2841d965e493b
skollr34p3r <48536777+skollr34p3r@users.noreply.github.com>
2022-01-20 14:56:00 -0500

    Create dropbox_setup_flash_drive.sh
    Creating this file to use for flash drive openvpn cert installs from the /home/pentest directory

---
+++ dropbox_setup_flash_drive.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+username=pentest
+
+#dropbox username
+#echo "Please enter dropbox username! (usually kali or pentest)"
+#read username
+#echo "USERNAME = $username"
+
+
+echo "Set new sudo password for $username user before running this script!"
+sleep 1
+
+#Sudo Check
+if [ `id -u` -eq 0 ]
+then
+        echo "Running as user with sudo privs :)"
+else
+        echo "Please run with standard (NON ROOT) user and sudo!"
+        exit 1
+fi
+
+echo "Disabling Sleep"
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+echo "Please enter the machine name which is also the name of the openvpn file without the extension"
+read -p 'Hostname: ' hostname
+
+filename=$hostname.ovpn
+
+# if ovpn file does not exist then exit
+if [ ! -f /home/pentest/$filename ]
+then
+    echo "$filename not found. Exiting!"
+    exit 1
+else
+    echo "File found. Copying now..."
+fi
+
+sudo cp /home/pentest/$filename /etc/openvpn/openvpn.conf
+#sudo rm /opt/openvpn/*
+sudo systemctl enable openvpn
+echo "Copied and enabled dropbox openvpn file!"
+
+#echo "Please enter new hostname (companyname):"
+#read hostname
+sudo hostnamectl set-hostname $hostname
+sed -I "s/127.0.1.1.*/127.0.1.1\t$hostname/g" /etc/hosts
+echo "Hostname set to $hostname"
+
+echo "UPDATING"
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing and enabling RDP"
+sudo apt install xrdp -y
+service xrdp start
+service xrdp-sesman start
+update-rc.d xrdp enable
+
+echo "Enabling SSH"
+sudo systemctl enable ssh.service
+systemctl start ssh.service
+
+echo "Enabling NTP"
+sudo timedatectl set-timezone America/New_York
+sudo systemctl enable ntp.service
+sudo systemctl start ntp.service
+
+echo "Cleaning Up..."
+sudo apt autoclean
+sudo apt -y autoremove
+
+echo "RECOMMEND REBOOT AFTER SCRIPT FINISHES……"
+sleep 3
